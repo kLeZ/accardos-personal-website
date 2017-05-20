492ff2b5976bd401e5320472e3e36e907eb06337
kLeZ <julius8774@gmail.com>
2017-05-20 12:30:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Intro
+This is my personal web site, in which I want to put some of the interesting things I've worked on in a while.
+
+# Architecture
+I wanted to do hands-on work in this architecture avoiding the boring "portfolio website" or "curriculum vitae web site". I wanted to do something different. And that's it, something different, a DevOps inspired, IaC inspired, microservice architecture. A very fun implementation of a boring personal web site.
---
+++ web-ui/src/main/java/it/aaccardo/webui/Application.java
@@ -0,0 +1,35 @@
+//    Copyright © 2017 Alessandro Accardo a.k.a. kLeZ <julius8774@gmail.com>
+//
+//    This file is part of AAccardo Personal WebSite.
+//
+//    AAccardo Personal WebSite is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    AAccardo Personal WebSite is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with AAccardo Personal WebSite.  If not, see <http://www.gnu.org/licenses/>.
+
+package it.aaccardo.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan("it.aaccardo.webui")
+public class Application {
+	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "web-ui");
+		SpringApplication.run(Application.class, args);
+	}
+}
